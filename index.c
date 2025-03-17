af8a49641e3d2cfa7cfbc6c104cb62259698c626
Aryan Bhardwaj <aryanbhardwaj4224@gmail.com>
2025-03-17 09:05:00 +0530

    changes

--- index.c
+++ index.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(){
-    printf("hello world");
+
+int main() {
+    char ch;
+    ch = getchar();  // Read a single character from input
+    printf("%c", ch); // Correct function name is 'printf', not 'print'
+
     return 0;
 }
