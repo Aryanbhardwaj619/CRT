f82bdd867ac33703f9679af253c45e032a8c7c07
Aryan Bhardwaj <aryanbhardwaj4224@gmail.com>
2025-03-19 11:03:47 +0530

    good question

--- README.md
+++
@@ -1,2 +0,0 @@
-# CRT
-CRT Work
--- index.c
+++ index.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    char ch;
-    ch = getchar();  // Read a single character from input
-    printf("%c", ch); // Correct function name is 'printf', not 'print'
+
+    int u;
+
+    printf("Enter total unit: ");
+    scanf("%d", &u);
+    switch (u){
+        case 1 ... 100 :
+        printf("%f",u*0.80);
+        break;
+        case 101 ... 300 :
+        printf("%f",u*1.50);
+        break;
+        case 301 ... 500 :
+        printf("%f",u*2.00);
+        break;
+        case 501 ... 700 :
+        printf("%f",u*3.00);
+        break;
+
+    }
 
     return 0;
+
 }
